f41e7121498ca9481074d61fd78eaebad3ee7a36
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-09-21 21:12:15 +0200

    Refactor `GlobalFixture`

--- test/Atata.ExtentReports.IntegrationTests/GlobalFixture.cs
+++ test/Atata.ExtentReports.IntegrationTests/GlobalFixture.cs
@@ -2,9 +2,6 @@
 
 public class GlobalFixture : AtataGlobalFixture
 {
-    protected override void ConfigureAtataContextGlobalProperties(AtataContextGlobalProperties globalProperties) =>
-        globalProperties.UseRootNamespaceOf<GlobalFixture>();
-
     protected override void ConfigureAtataContextBaseConfiguration(AtataContextBuilder builder)
     {
         builder.Sessions.AddWebDriver(x => x
